d48ad195b7f476ed594924f48d91bd3ae1135e39
Rahman <rahmanraynkunta@gmail.com>
2021-02-02 10:00:32 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// let button = document.querySelector('input');
+// button.addEventListener('click', changeColor);
+
+
+// function changeColor() {
+//     if (button.value === 'PressMe') {
+//         button.value = 'PressMe';
+//         paragraph.textContent = 'The color change'
+
+//     } else {
+//         button.value = 'PressMe';
+//         paragraph.textContent = 'The color change'
+//     }
+// }
